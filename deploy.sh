a3f3444cb68e89cbf788a0e2df5fe5684e933d2f
김도원 <kdwon7592@naver.com>
2023-07-11 09:28:19 +0900

    23.07.11 deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@ REPOSITORY=/home/ubuntu/deploy # 배포된 프로젝트 경로.
 
 cd $REPOSITORY # 이 경로로 이동해서 밑에 명령어들을 차례로 실행.
 
-sudo su
+npm install # 의존성 파일 설치.
 
-sudo npm install # 의존성 파일 설치.
-
-sudo npx pm2 reload all # 프로젝트에서 변경된 내용을 반영하기 위해 pm2를 reload.
+npx pm2 reload all # 프로젝트에서 변경된 내용을 반영하기 위해 pm2를 reload.

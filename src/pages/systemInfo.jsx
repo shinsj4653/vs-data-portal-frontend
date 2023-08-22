76f0c0e1e7d0b813643b3e0b40f09805476cfc58
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-08-22 11:48:07 +0900

    feat: 시스템 정보 UI 모두 반영 완료

--- src/pages/systemInfo.jsx
+++ src/pages/systemInfo.jsx
@@ -5,10 +5,8 @@ import { useOrgChartMain } from '../hooks/useOrgChart';
 import { useMetadataMainDataSet, useMetadataSubDataSet, useMetadataTableInfo, useMetadataTableSearch } from '../hooks/useMetaData';
 import systemInfo_background from '../assets/backgrounds/systemInfo_background.jpg';
 import service_pionada_log from '../assets/logos/service_pionada_logo.jpg';
-import Pagination from '../components/metaDataInfo/pagination';
 import Loading from '../components/loading';
 import { useLocation } from 'react-router-dom';
-import MainSearchBar from '../components/main/mainSearchBar';
 
 
 const SystemInfo = () => {
@@ -17,59 +15,24 @@ const SystemInfo = () => {
 	console.log(location.state);
 
 	const [orgData, setOrgData] = useState(null);
-	const mainDatasetRef = useRef(null);
-	
-	
     const [mainDatasetList, setMainDatasetList] = useState([]);
-    const [subDatasetList, setSubDatasetList] = useState([]);
-	const subDatasetRef = useRef(null);
 	
 	const [clickedNodeId, setClickedNodeId] = useState(null);
 	const [serviceName, setServiceName] = useState(location.state?.serviceName ?? '피어나다');
 
 	// 만약 mainCategory 있으면 그걸로, 없으면 '회원, 교사'로 초기화
-	const [selectedMainDataset, setSelectedMainDataset] = useState(location.state?.selectedMainDataset ?? '회원, 교사'); // ex) '회원, 교사'
-    const [selectedSubDataset, setSelectedSubDataset] = useState(location.state?.selectedSubDataset ?? '코치'); // ex) '코치'
-	const [tableInfoList, setTableInfoList] = useState([]);
-
 	
 	const itemsPerPage = 15;
 	const [currentPage, setCurrentPage] = useState(1);
-	const [totalPages, setTotalPages] = useState(0);
 	
-	const [scrollPosition, setScrollPosition] = useState(0);
 	
-	const [searchStandard, setSearchStandard] = useState('테이블ID & 테이블명');
 	const [searchCondition, setSearchCondition] = useState('table_id_or_name'); // [테이블ID & 이름, 기타
 	const [isSearch, setIsSearch] = useState(false);
-	const [searchValue , setSearchValue] = useState("");
-	const [searchResult, setSearchResult] = useState([]);
 	const [currentSearch, setCurrentSearch] = useState(null); // 현재 검색어
 
-	const [searchPageNo, setSearchPageNo] = useState(1); // 검색 결과 페이지 번호
-	const [searchAmountPerPage, setSearchAmountPerPage] = useState(15); // 검색 결과 페이지 당 개수
-
-	const searchQuery = useMetadataTableSearch(serviceName, searchCondition, currentSearch, searchPageNo, searchAmountPerPage);
-
-	
-
-	const handlePageChange = (list, pageNumber, isSearchPage) => {
-		isSearchPage ? setSearchPageNo(pageNumber) : setCurrentPage(pageNumber);
-		console.log("길이 ", list.length);
-		console.log("total ", totalPages);
-		console.log("현재 페이지 ", pageNumber);
-	};
-
-	useEffect(() => {
-
-		fetchResultData();
-		
-
-	}, [searchPageNo, searchAmountPerPage]);
-	
 	const startIndex = (currentPage - 1) * itemsPerPage;
 	const endIndex = startIndex + itemsPerPage;
-	const visibleItems = tableInfoList.slice(startIndex, endIndex);
+
 
 	// const handleDatasetColorChange
   
@@ -94,8 +57,6 @@ const SystemInfo = () => {
 	const handleNodeClick = (nodeId, nodeName, nodeDepth) => {
 		location.state = null;
 		setIsSearch(false);
-		setSearchResult([]);
-		setSearchValue("");
 
 		setClickedNodeId(nodeId);
 		setCurrentPage(1);
@@ -109,209 +70,28 @@ const SystemInfo = () => {
 	
 	const orgDataQuery = useOrgChartMain();
     const mainDatasetDataQuery = useMetadataMainDataSet(location.state?.serviceName ?? serviceName);
-    const subDatasetDataQuery = useMetadataSubDataSet(location.state?.serviceName ?? serviceName, location.state?.selectedMainDataset ?? selectedMainDataset);
-	const tableInfoDataQuery = useMetadataTableInfo(location.state?.serviceName ?? serviceName, location.state?.selectedMainDataset ?? selectedMainDataset, location.state?.selectedSubDataset ?? selectedSubDataset);
 	
-
-	const updateValue = (value) => {
-		setSearchValue(value);
-	}
-
-	const handleSearch = (value) => {
-		console.log(value.toLowerCase());
-		setCurrentSearch(value.toLowerCase());
-
-		if(searchValue == "") {
-			alert("검색어를 입력해주세요.");
-			return;
-		} else 
-			fetchResultData(); 
-
-		
-	}
-
-	const fetchResultData = async () => {
-		
-		const result = await searchQuery.refetch();
-		// console.log(result.data.data);
-		if(!result.isLoading) {
-			if (result.data.data.length == 0 ) {
-				setSearchResult([]);
-				alert("검색 결과가 없습니다.");
-				return;
-			}
-			setSearchResult(result.data.data);
-		}
-
-
-	}
 	const fetchData = async (param) => { 
-		setSearchResult([]);
-		if (param === "init") {
-			const [orgData, mainDataset, subDataset, tableInfoData] = await Promise.all([
-				orgDataQuery.refetch(),
-				mainDatasetDataQuery.refetch(),
-				subDatasetDataQuery.refetch(),
-				tableInfoDataQuery.refetch(),
-			]);
-
-			if (!orgData.isLoading && !mainDataset.isLoading && !subDataset.isLoading && !tableInfoData.isLoading) {
-				
-				const transformedData = transformData(orgData.data.data);
-				setOrgData(transformedData);
-				setMainDatasetList(mainDataset.data.data);
-				setSelectedMainDataset(location.state?.selectedMainDataset ?? mainDataset.data.data[0]);
-				
-				setSubDatasetList(subDataset.data.data);
-				setSelectedSubDataset(location.state?.selectedSubDataset ?? subDataset.data.data[0]);
-				setTableInfoList(tableInfoData.data.data);
-			}
-			
-
-		} else if (param === "serviceChange") {
-			
-			const [mainDataset, subDataset, tableInfoData]  = await Promise.all([
-				mainDatasetDataQuery.refetch(),
-				subDatasetDataQuery.refetch(),
-				tableInfoDataQuery.refetch()
-			]);
-
-			if (!mainDataset.isLoading && !subDataset.isLoading && !tableInfoData.isLoading) { 
-				setMainDatasetList(mainDataset.data.data);
-				setSelectedMainDataset(location.state?.selectedMainDataset ?? mainDataset.data.data[0]);
-
-				// 피어나다와 온리원초등 사이에서 이동할 때 중분류 명 및 메타 테이블 정보가 한 번에 안 불러와지는 오류가 존재하여, 
-				// 한 번더 렌더링 시켜줌
-				const subDatasetDataRefetch = await subDatasetDataQuery.refetch();
-				if (!subDatasetDataRefetch.isLoading) {
-					setSubDatasetList(subDatasetDataRefetch.data.data);
-					setSelectedSubDataset(location.state?.selectedSubDataset ?? subDatasetDataRefetch.data.data[0]);
-				}
-				const tableInfoDataRefetch = await tableInfoDataQuery.refetch();
-					if (!tableInfoDataRefetch.isLoading)
-						setTableInfoList(tableInfoDataRefetch.data.data);
-
-			}
-			
-		} else if(param === "mainCategoryChange") {
-			const [subDataset, tableInfoData]  = await Promise.all([
-				subDatasetDataQuery.refetch(),
-				tableInfoDataQuery.refetch()
-			]);
-			
-			if (!subDataset.isLoading && !tableInfoData.isLoading) {
-				setSubDatasetList(subDataset.data.data);
-				console.log(subDataset);
-				setSelectedSubDataset(location.state?.selectedSubDataset ?? subDataset.data.data[0]);
-				setTableInfoList(tableInfoData.data.data);
-				
-			} 
-		} else {
-			const tableInfoData = await tableInfoDataQuery.refetch();
-			if(!tableInfoData.isLoading)
-				setTableInfoList(tableInfoData.data.data);
-		}	
-
+		setMainDatasetList([]);
+	    const [orgData, mainDataset] = await Promise.all([
+	    	orgDataQuery.refetch(),
+	    	mainDatasetDataQuery.refetch(),
+	    ])  
+	    if (!orgData.isLoading && !mainDataset.isLoading) {
+        
+	    	const transformedData = transformData(orgData.data.data);
+	    	setOrgData(transformedData);
+	    	setMainDatasetList(mainDataset.data.data);
+	    }
     };
 
 	
     useEffect(() => {
-        fetchData("init");
-    }, []);
+        fetchData();
+    }, [serviceName]);
 
-	useEffect(() => {
-		fetchData("serviceChange");
-	}, [serviceName]);
 
-	useEffect(() => {
-		fetchData("mainCategoryChange");
-	}, [selectedMainDataset]);
-
-	useEffect(() => {
-		fetchData("subCategoryChange");
-	}, [selectedSubDataset]);
-
-	useEffect(() => {
-		fetchResultData();
-	}, [searchPageNo, currentSearch])
-
-    const handleMainDatasetColorChange = (child) => {
-		location.state = null;
-    	
-		setCurrentPage(1);
-		setSelectedMainDataset(child);
-		fetchData("subCategoryChange");
-		
-		const clickedButton = mainDatasetRef.current.querySelector(`button[data-child="${child}"]`);
-		if (clickedButton) {
-			// Scroll the view to the clicked element
-			  setScrollPosition(mainDatasetRef.current.scrollLeft); // Store the current scroll position
-			
-			clickedButton.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-			//clickedButton.scrollLeft = mainDatasetRef.current.scrollLeft;
-		  }
-	};
-	
-
-	// const [clickedSub, setClickedSub] = useState([]);
-
-	const handleSubDatasetColorChange = (child) => {
-		location.state = null;
-		const clickedButton = subDatasetRef.current.querySelector(`button[data-child="${child}"]`);
-		// const allButtons = subDatasetRef.current.querySelector(`button`);
-
-		// 모든 버튼들을 배열에 추가합니다.
-		// const allButtonsArray = Array.from(allButtons).map((button) => button.getAttribute('data-child'));
-		// setClickedSub(allButtonsArray);
-
-    	if (clickedButton) {
-    	  // Scroll the view to the clicked element
-    	  clickedButton.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    	}
-		
-		setCurrentPage(1);
-		setSelectedSubDataset(child);
-
-	}
-
-	const handleSearchStandardColorChange = (child) => {
-		setSearchStandard(child);
-		setSearchValue("");
-		setCurrentSearch(null);
-		setSearchResult([]);
-
-		if(child === "테이블ID & 테이블명") {
-			setSearchCondition("table_id_or_name");
-		} else if(child === "하위 주제") {
-			setSearchCondition("small_clsf_name");
-		}
-	}
-
-	const highlightLetters = (tableText, currentSearch) => {
-		const regex = new RegExp(currentSearch, 'gi');
-    	const matches = tableText.match(regex);
-
-    	if (!matches) return <span className="text-gray-400 font-bold text-sm">{tableText}</span>;
-
-    	const parts = tableText.split(regex);
-    	const highlightedParts = [];
-
-    	parts.forEach((part, index) => {
-    	    highlightedParts.push(<span className="text-gray-400 font-bold text-sm" key={index}>{part}</span>);
-
-    	    if (index < parts.length - 1) {
-    	        highlightedParts.push(
-    	            <span key={`highlight-${index}`} className="inline-block px-1 py-0.5 text-gray-400 bg-yellow-300 font-extrabold text-sm">
-    					{matches[index]}
-					</span>
-    	        );
-    	    }
-    	});
-
-    	return <span>{highlightedParts}</span>;
-	}
-
-    if (!orgData || orgDataQuery.isLoading || mainDatasetDataQuery.isLoading || subDatasetDataQuery.isLoading || tableInfoDataQuery.isLoading) {
+    if (!orgData || orgDataQuery.isLoading || mainDatasetDataQuery.isLoading) {
 		return (
 			<Loading></Loading>
 		);
@@ -343,14 +123,12 @@ const SystemInfo = () => {
 						isMap={true}
 						isSearch={isSearch}
 						setIsSearch={setIsSearch}
-						setSearchValue={setSearchValue}
-						setSearchResult={setSearchResult}
 					/>
 					
 					{serviceName === "피어나다" ?
-						(<div className='flex w-full justify-center items-center'>
-                            <div className='flex w-1/3 justify-center pt-20 h-full bg-white p-100'>
-                                <div className='flex flex-col justify-center items-center space-y-10 w-9/12 h-1/3 bg-white drop-shadow-2xl hover:shadow-xl transition duration-300'>
+						(<div className='flex w-full justify-center items-center -ml-5'>
+                            <div className='flex w-2/5 justify-center pt-20 h-full bg-white p-100'>
+                                <div className='flex flex-col justify-center items-center space-y-10 w-9/12 h-1/3 bg-white drop-shadow-2xl transition duration-300'>
                                     <div className='flex w-3/4 h-4/6 bg-contain bg-center bg-no-repeat' style={{backgroundImage: `url(${ service_pionada_log })`}} />
                                     <div className='flex flex-col items-center'>
                                         <p className='font-semibold	'>심리 상담부터 학습 코칭까지 하나로 결합한</p>
@@ -359,43 +137,54 @@ const SystemInfo = () => {
                                 </div>
                             </div>
 
-                            <div className='flex flex-col w-2/3 h-full'>
-                                <div className='flex flex-col w-full drop-shadow-xl'>
+                            <div className='flex flex-col w-3/5 h-full pt-20 mb-15 -ml-10'>
+                                <div className='flex flex-col w-full'>
                                     <div className='flex rounded-t-xl h-10'>
-                                        <div className='flex justify-center items-center w-1/5 bc-[#F5F7FA] border font-black text-[#0975DA] rounded-tl-xl text-sm'>데이터베이스 정보</div>
-                                        <div className='flex justify-center items-center w-4/5 bc-white border font-black rounded-tr-xl text-sm'>Maria DB</div>
+                                        <div className='flex justify-center items-center w-1/4 bg-gray-100 border font-black text-[#0975DA] rounded-tl-xl text-sm'>데이터베이스 정보</div>
+                                        <div className='flex justify-center items-center w-3/4 bc-white border font-black rounded-tr-xl text-sm'>Maria DB</div>
                                     </div>
                                     <div className='flex h-10'>
-                                        <div className='flex justify-center items-center w-1/5 bc-[#F5F7FA] border font-black text-[#0975DA] text-sm'>운영 환경</div>
-                                        <div className='flex justify-center items-center w-4/5 bc-white border font-black text-sm'>Amazon Web Service Cloud</div>
+                                        <div className='flex justify-center items-center w-1/4 bg-gray-100 border font-black text-[#0975DA] text-sm'>운영 환경</div>
+                                        <div className='flex justify-center items-center w-3/4 bc-white border font-black text-sm'>Amazon Web Service Cloud</div>
                                     </div>
                                     <div className='flex h-10'>
-                                        <div className='flex justify-center items-center w-1/5 bc-[#F5F7FA] border  font-black text-[#0975DA] text-sm'>홈페이지</div>
-                                        <div className='flex justify-center items-center w-4/5 bc-white border  font-black text-sm'>https://www.pionada.com</div>
+                                        <div className='flex justify-center items-center w-1/4 bg-gray-100 border  font-black text-[#0975DA] text-sm'>홈페이지</div>
+                                        <div className='flex justify-center items-center w-3/4 bc-white border  font-black text-sm'>https://www.pionada.com</div>
                                     </div>
                                     <div className='flex h-10'>
-                                        <div className='flex pl-5 items-center w-4/5 bc-white border font-black text-lg'>담당자 정보</div>
+                                        <div className='flex pl-5 items-center  w-full bc-white border font-black text-lg'>담당자 정보</div>
                                     </div>
                                     <div className='flex h-10'>
-                                        <div className='flex justify-center items-center w-1/5 bc-[#F5F7FA] border  font-black text-[#0975DA] text-sm'>기획 관리부서명/담당자</div>
-                                        <div className='flex justify-center items-center w-4/5 bc-white border  font-black text-sm'>서비스 Cell / 김영갑</div>
+                                        <div className='flex justify-center items-center w-1/4 bg-gray-100 border  font-black text-[#0975DA] text-sm'>기획 관리부서명/담당자</div>
+                                        <div className='flex justify-center items-center w-3/4 bc-white border  font-black text-sm'>서비스 Cell / 김영갑</div>
                                     </div>
                                     <div className='flex h-10'>
-                                        <div className='flex justify-center items-center w-1/5 bc-[#F5F7FA] border font-black text-[#0975DA] text-sm'>개발 관리부서명/담당자</div>
-                                        <div className='flex justify-center items-center w-4/5 bc-white border font-black text-sm'>서비스 Cell / 박건규</div>
+                                        <div className='flex justify-center items-center w-1/4 bg-gray-100 border font-black text-[#0975DA] text-sm'>개발 관리부서명/담당자</div>
+                                        <div className='flex justify-center items-center w-3/4 bc-white border font-black text-sm'>서비스 Cell / 박건규</div>
                                     </div>
                                     <div className='flex h-10'>
-                                        <div className='flex justify-center items-center w-1/5 bc-[#F5F7FA] border font-black text-[#0975DA] rounded-bl-xl text-sm'>인프라 관리부서명/담당자</div>
-                                        <div className='flex justify-center items-center w-4/5 bc-white border font-black rounded-br-xl text-sm'>IT인프라 Cell / 임종규, 고영원</div>
+                                        <div className='flex justify-center items-center w-1/4 bg-gray-100 border font-black text-[#0975DA] rounded-bl-xl text-sm'>인프라 관리부서명/담당자</div>
+                                        <div className='flex justify-center items-center w-3/4 bc-white border font-black rounded-br-xl text-sm'>IT인프라 Cell / 임종규, 고영원</div>
                                     </div>
                                 </div>
-                                <hr className="h-0.5 bg-black mt-15 mb-15" />
-                                <div>
-                                    <p className='font-black text-m'>주요 데이터셋</p>
-                                    <div className='flex flex-wrap w-full h-48 drop-shadow-xl bc-green-500'>
-                                        hifawefawf
-                                        awfaw
-                                        wafw
+                                <hr className="h-0.5 bg-black mt-5 mb-5"></hr>
+                                <div className='flex flex-col w-full h-2/5'>
+                                    <p className='font-black text-m mb-5'>주요 데이터셋</p>
+                                    <div className='flex justify-center items-center h-32 w-full bg-white drop-shadow-l border-2 rounded-l px-20'>
+                                        <div className='flex flex-wrap w-3/4 justify-center items-center'>
+                                        {
+                                            mainDatasetList?.map((child, idx) => {
+                                                return (
+                                                    <button
+                                                        className='flex justify-center items-center w-1/4 bg-gray-100 border font-black text-sm m-1 mx-3 p-3 drop-shadow-l hover:shadow-xl transition duration-300'
+                                                        key={child}
+                                                    >
+                                                        {child}
+                                                    </button>
+                                                )
+                                            })
+                                        }
+                                        </div>
                                     </div>
                                 </div>
                             </div>

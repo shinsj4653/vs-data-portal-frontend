c490f1d85ec8ca54b2661e8f5ba0baa20327d2ad
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-08-24 15:16:16 +0900

    fix: useSercviceSystemInfo -> useServiceSystemInfo
    hooks 명 오타 수정

--- src/hooks/useSysInfo.js
+++ src/hooks/useSysInfo.js
@@ -1,7 +1,7 @@
 import { useQuery } from 'react-query';
 import { fetchServiceSystemInfo } from '../api/sysInfoApi';
 
-export const useSercviceSystemInfo = (serviceName) => {
+export const useServiceSystemInfo = (serviceName) => {
     return useQuery(['serviceSystemInfo', serviceName], () => fetchServiceSystemInfo(serviceName));
   };
   

e27a7a6f278440c292e3d7b985e538e746584614
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-11-15 11:21:49 +0900

    feat: localhost 경로로 테스트

--- src/api/dataMapApi.js
+++ src/api/dataMapApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 /* 로컬 테스트용 */
-// axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = "http://localhost:8080";
 
 /* 개발 테스트용 */
-axios.defaults.baseURL = "http://vs-dpc-alb-01-487593038.ap-northeast-2.elb.amazonaws.com";
+// axios.defaults.baseURL = "http://vs-dpc-alb-01-487593038.ap-northeast-2.elb.amazonaws.com";
 
 axios.defaults.withCredentials = true; // withCredentials 전역 설정
 

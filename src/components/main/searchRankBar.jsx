fec763fc6d7fd21a9b59e1664ba9add82002d448
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-12-05 18:57:46 +0900

    fix: 검색 기록 없을 경우의 글씨 위치 수정

--- src/components/main/searchRankBar.jsx
+++ src/components/main/searchRankBar.jsx
@@ -14,14 +14,14 @@ const SearchRankBar = ({ searchRankResult }) => {
     }, [searchRankResult]);
   
     return (
-      <div className="w-1/5 h-7 overflow-hidden ">
+      <div className="w-1/5 h-7 overflow-hidden">
         {searchRankResult && searchRankResult.length > 0 ? (
           <ul
-            className="list-none p-0 m-0 transition-transform duration-400 ease-in-out"
+            className="w-full list-none p-0 m-0 transition-transform duration-400 ease-in-out"
             style={{ transform: `translateY(-${currentIndex * 24}px)` }}
           >
             {searchRankResult.map((item, index) => (
-              <li key={index} className="h-34 px-4 font-bold flex items-center">
+              <li key={index} className="justify-center h-34 px-4 font-bold flex items-center">
                 <span className="font-bold text-[#00B2E2]">{index + 1}.</span>
                 <span className="font-bold text-[#00B2E2]">{item.keyword}</span>
               </li>
@@ -29,7 +29,7 @@ const SearchRankBar = ({ searchRankResult }) => {
           </ul>
         ) : (
           <div className="flex items-center justify-center w-full h-full">
-            <span className="text-[#00B2E2]"> 검색 기록이 없습니다. </span>
+            <span className="font-bold text-[#00B2E2]"> 검색 기록이 없습니다. </span>
           </div>
         )}
       </div>
